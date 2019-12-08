e7e067953d044701a3c10418d5e3672ddc14e1d5
psynr <psynr@nottingham.ac.uk>
2019-12-08 18:22:23 +0000

    Flyweight obstacles

--- src/model/ObstacleFactory.java
+++ src/model/ObstacleFactory.java
@@ -1,16 +1,25 @@
 package model;
 
+import java.util.HashMap;
+
 import model.LevelCreator.ActorTypes;
 
 public class ObstacleFactory {
-	public static Obstacle getTruck(ActorTypes typeOfActor, int xpos, int ypos, double s){
-		if(typeOfActor == ActorTypes.TruckBig){
-			return new TruckBig(xpos,ypos,s);
-		}
-		if(typeOfActor == ActorTypes.TruckSmall){
-			return new TruckSmall(xpos,ypos,s);
-		}
+    private static HashMap <String, Obstacle> hm = new HashMap<String, Obstacle>(); 
 
-		return null;
+	public static Obstacle getTruck(ActorTypes typeOfActor, int xpos, int ypos, double s){
+        Obstacle p = null; 
+        if (hm.containsKey(typeOfActor)) 
+            p = hm.get(typeOfActor); 
+        else
+        { 
+			if(typeOfActor == ActorTypes.TruckBig){
+				p = new TruckBig(xpos,ypos,s);
+			}
+			if(typeOfActor == ActorTypes.TruckSmall){
+				p = new TruckSmall(xpos,ypos,s);
+			}
+        }
+		return p;
 	}
 }
