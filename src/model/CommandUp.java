8ab4fb3403e88c17a662f51a79428700795d890e
psynr <psynr@nottingham.ac.uk>
2019-12-08 12:14:45 +0000

    Adding unstaged

--- src/model/CommandUp.java
+++ src/model/CommandUp.java
@@ -1,5 +1,16 @@
 package model;
 
-public class CommandUp {
-
+public class CommandUp implements Command{
+	Animal animal; 
+	  
+    // The constructor is passed the light it 
+    // is going to control. 
+    public CommandUp(Animal animal) 
+    { 
+       this.animal = animal; 
+    } 
+    
+    public void execute() 
+    { 
+    } 
 }
