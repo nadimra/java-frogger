b924f9f187c02dc7dd228007195643729f80cfff
psynr <psynr@nottingham.ac.uk>
2019-12-08 18:48:49 +0000

    Turtle factory flyweight

--- src/model/TurtleFactory.java
+++ src/model/TurtleFactory.java
@@ -1,18 +1,26 @@
 package model;
 
+import java.util.HashMap;
+
 import model.LevelCreator.ActorTypes;
 
 public class TurtleFactory {
+		private static HashMap <String, Turtle> hm = new HashMap<String, Turtle>(); 
 
 		public static Turtle getTurtle(ActorTypes typeOfActor, int xpos, int ypos, double s){
-			if(typeOfActor == ActorTypes.NormalTurtle){
-				return new NormalTurtle(xpos,ypos,s);
-			}
-			if(typeOfActor == ActorTypes.WetTurtle){
-				return new WetTurtle(xpos,ypos,s);
-			}
-
-			return null;
+	        Turtle p = null; 
+	        if (hm.containsKey(typeOfActor)) 
+	            p = hm.get(typeOfActor); 
+	        else
+	        { 
+				if(typeOfActor == ActorTypes.NormalTurtle){
+					p = new NormalTurtle(xpos,ypos,s);
+				}
+				if(typeOfActor == ActorTypes.WetTurtle){
+					p = new WetTurtle(xpos,ypos,s);
+				}
+	        }
+	        return p;
 		}
 		
 		
