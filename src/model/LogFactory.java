9be5bc248270f8ab9f559e861b43f4bb7c7a223c
psynr <psynr@nottingham.ac.uk>
2019-12-08 18:19:35 +0000

    Flyweight for logs

--- src/model/LogFactory.java
+++ src/model/LogFactory.java
@@ -1,20 +1,30 @@
 package model;
 
+import java.util.HashMap;
+
 import model.LevelCreator.ActorTypes;
 
 public class LogFactory {
 
+    private static HashMap <String, Log> hm = new HashMap<String, Log>(); 
+    
 	public static Log getLog(ActorTypes typeOfActor, int xpos, int ypos, double s){
-		if(typeOfActor == ActorTypes.LogBig){
-			return new LogBig(xpos,ypos,s);
-		}
-		if(typeOfActor == ActorTypes.LogMedium){
-			return new LogMedium(xpos,ypos,s);
-		}
-		if(typeOfActor == ActorTypes.LogSmall){
-			return new LogSmall(xpos,ypos,s);
-		}
-		return null;
+        Log p = null; 
+        if (hm.containsKey(typeOfActor)) 
+            p = hm.get(typeOfActor); 
+        else
+        { 
+			if(typeOfActor == ActorTypes.LogBig){
+				p = new LogBig(xpos,ypos,s);
+			}
+			if(typeOfActor == ActorTypes.LogMedium){
+				p =  new LogMedium(xpos,ypos,s);
+			}
+			if(typeOfActor == ActorTypes.LogSmall){
+				p = new LogSmall(xpos,ypos,s);
+			}
+        }
+		return p;
 	}
 	
 	
